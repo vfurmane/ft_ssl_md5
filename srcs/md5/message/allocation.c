fafc86f16b77543272b63d8ac0a9fffbd66dbcaa
vfurmane <vfurmane@student.42.fr>
2024-10-17 12:54:14 +0200

    feat: add initial string length in message buffer

--- srcs/md5/message/allocation.c
+++ srcs/md5/message/allocation.c
@@ -2,13 +2,15 @@
 
 md5_padded_buffer_t allocate_buffer_from_string_with_padding(const char *str) {
   const size_t len = ft_strlen(str);
-  const size_t remainder_chunks_nbr = len % 512;
+  const size_t remainder_chunks_nbr = (len + 1 + 64) % 512;
   const size_t full_chunks_nbr = len / 512 + (remainder_chunks_nbr > 0 ? 1 : 0);
-  const size_t new_len = full_chunks_nbr * 512;
+  const size_t new_len = full_chunks_nbr * 512 / 8;
 
   char *b = malloc(new_len);
   ft_memcpy(b, str, sizeof(*b) * len);
   ft_bzero(b + len, new_len - len);
+  b[len] = 1;
+  (((size_t *)(b))[(new_len - 8) / 8]) = len;
 
   const md5_padded_buffer_t buffer = {.len = new_len, .b = b};
 
