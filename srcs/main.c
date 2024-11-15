76abd54384bf5703ac8c769818b392e6b89e5a03
vfurmane <vfurmane@student.42.fr>
2024-11-15 17:13:35 +0100

    feat: check for NULL buffer in main function

--- srcs/main.c
+++ srcs/main.c
@@ -3,6 +3,11 @@
 
 int main(void) {
   md5_message_t message = allocate_message_from_string("hello, world!");
+  if (message.b == NULL) {
+    const char *error_message = "cannot allocate buffer";
+    write_stderr(error_message, ft_strlen(error_message));
+    return 1;
+  }
 
   const md5_hash_t hash = md5_hash(message);
 
