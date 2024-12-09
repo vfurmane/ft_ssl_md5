751dfbae1a032879ef8d146b87da0b5b75a29fbb
vfurmane <vfurmane@student.42.fr>
2024-12-09 17:36:43 +0100

    feat: parse -s argument

---
+++ includes/args.h
@@ -0,0 +1,9 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+typedef enum arg_parser_state_e {
+  INITIAL,
+  STRING,
+} arg_parser_state_t;
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,14 +1,27 @@
+#include "args.h"
 #include "md5/hash.h"
 #include "print.h"
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
+  if (argc < 2) {
     return 1;
   }
 
-  const md5_hash_t hash = md5_hash_static_string(argv[1]);
-
-  print_md5_hash(hash);
+  arg_parser_state_t arg_parser_state = INITIAL;
+  for (int i = 1; i < argc; ++i) {
+    if (arg_parser_state == INITIAL) {
+      if (ft_strcmp(argv[i], "-s") == 0) {
+        arg_parser_state = STRING;
+      } else {
+        return 1;
+      }
+    } else if (arg_parser_state == STRING) {
+      const md5_hash_t hash = md5_hash_static_string(argv[i]);
+      print_md5_hash(hash);
+    } else {
+      return 1;
+    }
+  }
 
   return 0;
 }
