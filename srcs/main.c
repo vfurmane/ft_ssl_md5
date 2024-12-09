a74a15ab1621a6fa1d8ec7c4e5e4c69a275dceef
vfurmane <vfurmane@student.42.fr>
2024-12-09 16:44:59 +0100

    feat: read hashed message from argv[1]

--- srcs/main.c
+++ srcs/main.c
@@ -2,10 +2,11 @@
 #include "print.h"
 
 int main(int argc, char **argv) {
-  (void)argc;
-  (void)argv;
+  if (argc != 2) {
+    return 1;
+  }
 
-  const md5_hash_t hash = md5_hash_static_string("hello, world!");
+  const md5_hash_t hash = md5_hash_static_string(argv[1]);
 
   print_md5_hash(hash);
 
