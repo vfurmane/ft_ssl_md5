aaf09ccd8089605f7b3bba10e4334647c803857e
vfurmane <vfurmane@student.42.fr>
2024-12-09 11:13:01 +0100

    feat: hash a static buffer instead of a dynamically allocated one

--- srcs/main.c
+++ srcs/main.c
@@ -2,21 +2,20 @@
 #include "print.h"
 
 int main(void) {
-  md5_message_t message = allocate_message_from_string("hello, world!");
-  if (message.b == NULL) {
-    const char *error_message = "cannot allocate buffer";
-    write_stderr(error_message, ft_strlen(error_message));
-    return 1;
-  }
-
+  // "hello, world!", with md5 padding
+  uint8_t buffer[] = {104, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108,
+                      100, 33,  128, 0,   0,   0,  0,  0,   0,   0,   0,
+                      0,   0,   0,   0,   0,   0,  0,  0,   0,   0,   0,
+                      0,   0,   0,   0,   0,   0,  0,  0,   0,   0,   0,
+                      0,   0,   0,   0,   0,   0,  0,  0,   0,   0,   0,
+                      0,   104, 0,   0,   0,   0,  0,  0,   0};
+  md5_message_t message = {.chunks = 1, .b = (uint32_t *)buffer};
   md5_hash_t base_hash = {
       .a = 0x67452301, .b = 0xefcdab89, .c = 0x98badcfe, .d = 0x10325476
   };
   const md5_hash_t hash = md5_hash(message, base_hash);
 
   print_md5_hash(hash);
-
-  destroy_message(&message);
 
   return 0;
 }
