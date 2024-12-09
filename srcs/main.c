1abaaa99b8b0200a84330495ebc036738e50e2a1
vfurmane <vfurmane@student.42.fr>
2024-12-09 12:32:05 +0100

    refactor: use pad_chunk function instead of manually padding temporary message

--- srcs/main.c
+++ srcs/main.c
@@ -3,13 +3,9 @@
 
 int main(void) {
   // "hello, world!", with md5 padding
-  uint8_t buffer[] = {104, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108,
-                      100, 33,  128, 0,   0,   0,  0,  0,   0,   0,   0,
-                      0,   0,   0,   0,   0,   0,  0,  0,   0,   0,   0,
-                      0,   0,   0,   0,   0,   0,  0,  0,   0,   0,   0,
-                      0,   0,   0,   0,   0,   0,  0,  0,   0,   0,   0,
-                      0,   104, 0,   0,   0,   0,  0,  0,   0};
-  md5_message_t message = {.chunks = 1, .b = (uint32_t *)buffer};
+  uint8_t buffer[BUFFER_BITS_NBR / CHAR_BIT] = {104, 101, 108, 108, 111, 44, 32,
+                                                119, 111, 114, 108, 100, 33};
+  md5_message_t message = {.chunks = 1, .b = (uint32_t *)pad_chunk(buffer, 13)};
   md5_hash_t base_hash = {
       .a = 0x67452301, .b = 0xefcdab89, .c = 0x98badcfe, .d = 0x10325476
   };
