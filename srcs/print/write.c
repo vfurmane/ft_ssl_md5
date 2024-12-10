e3ec7f1d58d883ff916cb77d2f2be01b23f26385
vfurmane <vfurmane@student.42.fr>
2024-12-10 12:23:27 +0100

    feat: add putstr_stdout function

--- srcs/print/write.c
+++ srcs/print/write.c
@@ -4,6 +4,11 @@ size_t write_stdout(const void *buf, size_t n) {
   return write(STDOUT_FILENO, buf, n);
 }
 
+size_t putstr_stdout(const char *str) {
+  const size_t len = ft_strlen(str);
+  return write_stdout(str, len);
+}
+
 size_t write_stderr(const void *buf, size_t n) {
   return write(STDERR_FILENO, buf, n);
 }
