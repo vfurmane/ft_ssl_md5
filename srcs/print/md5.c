a76319b42fd4bc00d2e31241bd15dec0510f2039
vfurmane <vfurmane@student.42.fr>
2024-12-10 12:52:19 +0100

    feat: write MD5 prefix only with string

--- srcs/print/md5.c
+++ srcs/print/md5.c
@@ -1,9 +1,14 @@
 #include "print.h"
 
 void print_formatted_md5_hash(
-    const char *str, md5_hash_t hash, uint8_t with_quotes
+    const char *str, md5_hash_t hash, uint8_t with_quotes,
+    uint8_t with_md5_prefix
 ) {
-  putstr_stdout("MD5 (");
+  if (with_md5_prefix) {
+    putstr_stdout("MD5 (");
+  } else {
+    putstr_stdout("(");
+  }
   if (with_quotes) {
     putstr_stdout("\"");
   }
@@ -11,15 +16,20 @@ void print_formatted_md5_hash(
   if (with_quotes) {
     putstr_stdout("\"");
   }
-  putstr_stdout(") = ");
+  putstr_stdout(")");
+  if (with_md5_prefix) {
+    putstr_stdout(" = ");
+  } else {
+    putstr_stdout("= ");
+  }
   print_md5_hash(hash);
   putstr_stdout("\n");
 }
 
 void print_md5_hashed_string(const char *str, md5_hash_t hash) {
-  return print_formatted_md5_hash(str, hash, 1);
+  return print_formatted_md5_hash(str, hash, 1, 1);
 }
 
 void print_md5_hashed_stdin(md5_hash_t hash) {
-  return print_formatted_md5_hash("stdin", hash, 0);
+  return print_formatted_md5_hash("stdin", hash, 0, 0);
 }
