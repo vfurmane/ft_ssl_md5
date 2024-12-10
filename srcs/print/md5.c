856d8d87c7bd8c52d65f5ef5ff1854e6687be294
vfurmane <vfurmane@student.42.fr>
2024-12-10 12:49:01 +0100

    refactor: move out the print_formatted_md5_hash function

--- srcs/print/md5.c
+++ srcs/print/md5.c
@@ -1,9 +1,21 @@
 #include "print.h"
 
-void print_md5_hashed_string(const char *str, md5_hash_t hash) {
-  putstr_stdout("MD5 (\"");
+void print_formatted_md5_hash(
+    const char *str, md5_hash_t hash, uint8_t with_quotes
+) {
+  putstr_stdout("MD5 (");
+  if (with_quotes) {
+    putstr_stdout("\"");
+  }
   putstr_stdout(str);
-  putstr_stdout("\") = ");
+  if (with_quotes) {
+    putstr_stdout("\"");
+  }
+  putstr_stdout(") = ");
   print_md5_hash(hash);
   putstr_stdout("\n");
+}
+
+void print_md5_hashed_string(const char *str, md5_hash_t hash) {
+  return print_formatted_md5_hash(str, hash, 1);
 }
