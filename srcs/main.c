fcb99590bf8c1144ed6677aca7c3b6eeb57b7742
vfurmane <vfurmane@student.42.fr>
2024-12-11 10:02:39 +0100

    refactor: add an else condition instead of skipping the loop

--- srcs/main.c
+++ srcs/main.c
@@ -15,30 +15,30 @@ int main(int argc, char **argv) {
         return 1;
       }
       print_md5_hashed_stdin(ret.hash);
-    }
-
-    arg_parser_state_t arg_parser_state = INITIAL;
-    for (int i = 2; i < argc; ++i) {
-      if (arg_parser_state == INITIAL) {
-        if (ft_strcmp(argv[i], "-s") == 0) {
-          arg_parser_state = STRING;
-        } else if (ft_strcmp(argv[i], "-p") == 0) {
-          const maybe_md5_hash_t ret = md5_hash_fd(STDIN_FILENO, 1);
-          if (!ret.some) {
-            putstr_stderr("error while reading stdin");
+    } else {
+      arg_parser_state_t arg_parser_state = INITIAL;
+      for (int i = 2; i < argc; ++i) {
+        if (arg_parser_state == INITIAL) {
+          if (ft_strcmp(argv[i], "-s") == 0) {
+            arg_parser_state = STRING;
+          } else if (ft_strcmp(argv[i], "-p") == 0) {
+            const maybe_md5_hash_t ret = md5_hash_fd(STDIN_FILENO, 1);
+            if (!ret.some) {
+              putstr_stderr("error while reading stdin");
+              return 1;
+            }
+            print_md5_hash(ret.hash);
+            putstr_stdout("\n");
+          } else {
             return 1;
           }
-          print_md5_hash(ret.hash);
-          putstr_stdout("\n");
+        } else if (arg_parser_state == STRING) {
+          const md5_hash_t hash = md5_hash_static_string(argv[i]);
+          print_md5_hashed_string(argv[i], hash);
+          arg_parser_state = INITIAL;
         } else {
           return 1;
         }
-      } else if (arg_parser_state == STRING) {
-        const md5_hash_t hash = md5_hash_static_string(argv[i]);
-        print_md5_hashed_string(argv[i], hash);
-        arg_parser_state = INITIAL;
-      } else {
-        return 1;
       }
     }
   }
